c41d6199ab13d53bd1fad5709f467c4dfe204924
OlenaRomanko <148877214+OlenaRomanko@users.noreply.github.com>
2023-11-12 20:04:08 +0200

    Labaratorna1.2

---
+++ Laboratorni/Lab.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x, y;
+    cout << "Введіть число x: ";
+    cin >> x;
+
+    y = ((2 * pow(x, 2) - pow(sin(x), 2)) / (cos(2 * x) + pow(x, 2)))
+        - ((x + 1) / (log(x)));
+    cout << "Результат y=" << y;
+
+    return 0;
+}
