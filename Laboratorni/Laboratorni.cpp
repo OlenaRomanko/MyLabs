423b1e0276303f5d7da0b6619ac6a9f4a33c631b
OlenaRomanko <148877214+OlenaRomanko@users.noreply.github.com>
2023-11-12 20:29:08 +0200

    Labaratorna4-5.1

---
+++ Laboratorni/Laboratorni.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	int s = 0, i;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	for (i = 5; i <= 50; i++)
+	{
+		s += pow(i, 3);
+	}
+	cout << "s=" << s << endl;
+
+	return 0;
+}
