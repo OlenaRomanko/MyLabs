7ff179d01358ebe946bf8b9780cbd51fddee6017
OlenaRomanko <148877214+OlenaRomanko@users.noreply.github.com>
2023-11-12 20:25:42 +0200

    Labaratorna2.2

---
+++ Laboratorni/Laboratorni.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int number;
+	cout << "Введіть номер маршруту трамвая: ";
+	cin >> number;
+
+	string text = "\nТрамвай рухається по маршруту: ";
+
+	switch (number) {
+	case 1:
+		cout << text << "Муніципальний ринок - Електромережа";
+		break;
+
+	case 2:
+		cout << text << "Барське шосе - Вишенька";
+		break;
+
+	case 3:
+		cout << text << "Електромережа - Вишенька";
+		break;
+
+	case 4:
+		cout << text << "Муніципальний ринок - Барське шосе";
+		break;
+
+	case 5:
+		cout << text << "Електромережа - Барське шосе";
+		break;
+
+	case 6:
+		cout << text << "Вишенька - Муніципальний ринок";
+		break;
+
+	default: cout << "Такого маршруту не існує!";
+	}
+}
