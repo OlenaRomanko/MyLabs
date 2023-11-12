d73b36b6070d38e43a6c0ad07f568e3c54e89ee1
OlenaRomanko <148877214+OlenaRomanko@users.noreply.github.com>
2023-11-12 20:32:54 +0200

    Laboratorna6.2

---
+++ Laboratorni/Laboratorni.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a0 = 1, a1 = 2;
+
+	cout << "a0 = " << a0 << "\n";
+	cout << "a1 = " << a1 << "\n";
+
+	for (int i = 2; i <= 12; i++) {
+		int a2 = a1 - a0;
+		a0 = a1;
+		a1 = a2;
+		cout << "a" << i << " = " << a2 << "\n";
+	}
+}
