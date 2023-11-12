4117d3cfb708c40964e3b472ffe69aad568e9717
OlenaRomanko <148877214+OlenaRomanko@users.noreply.github.com>
2023-11-12 20:32:04 +0200

    Laboratorna4-5.2

---
+++ Laboratorni/Laboratorni.cpp
@@ -0,0 +1,27 @@
+﻿#include <cmath>
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double a = 0.0; // Початок відрізку
+	double b = 5.0; // Кінець відрізку
+	int k;          // Кількість точок
+	cout << "Введіть натуральне число: k=";
+	cin >> k;
+	double h = (b - a) / k;
+
+	for (int i = a; i <= b; i++) {
+		double xi = a + i * h;
+		double y = sin(pow(xi, 2) + 4);
+		cout << "x[" << i << "] = " << xi << ",     y[" << i << "] = " << y << endl;
+	}
+
+	return 0;
+}
