d4bf8aa596cf4f3ccd13e8b5cc479bace5a0b110
OlenaRomanko <148877214+OlenaRomanko@users.noreply.github.com>
2023-11-12 20:24:43 +0200

    Labaratorna2.1

---
+++ Laboratorni/Laboratorni.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x;
+	cout << "Введіть число x: ";
+	cin >> x;
+
+	double y;
+
+	if (x < 0) {
+		y = pow(sin(x), 2);
+	}
+	else if (x >= 0) {
+		y = log(x + 1);
+	}
+
+	cout << "\nФункція y=" << y;
+
+}
+
+
